07c00b3b90667fac29f8186f02e4b9508c2b22cb
seonhannn <seon.hannn@gmail.com>
2022-10-20 21:00:22 +0900

    weather info page css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import axios from "axios";
 import {useState} from "react";
+import "./App.css";
 
 function App() {
 
   const [location, setLocation] = useState("");
   const [result, setResult] = useState({});
   const API_KEY = "614f734515b5ebb1a40e59ef2c368a27";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`;
 
   const searchWeather = async (e) => {
     if(e.key === 'Enter') {
@@ -24,21 +25,36 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="weather-container">
+      <div className="weather-input">
       <input 
-      placeholder="도시를 입력하세요" 
-      value={location}
-      onChange={(e) => setLocation(e.target.value)}
-      type="text"
-      onKeyDown={searchWeather}>
-      </input>
+        placeholder="도시를 입력하세요" 
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+        type="text"
+        onKeyDown={searchWeather}>
+        </input>
       {Object.keys(result).length !== 0 && (
-          <div>
-            <div className="city">{result.data.name}</div>
-            <div className="temperature">{result.data.main.temp}</div>
-            <div className="sky">{result.data.weather[0].main}</div>
+          <div className="weather-data">
+            <div className="weather-data-left">
+              <div className="title">지역</div>
+              <div className="title">온도</div>
+              <div className="title">상태</div>
+            </div>
+            <div className="weather-data-right">
+              <div className="city">
+                {result.data.name}
+              </div>
+              <div className="temperature">
+                {result.data.main.temp}
+              </div>
+              <div className="sky">
+                {result.data.weather[0].main}
+              </div>
+            </div>
           </div>
         )}
+      </div>
     </div>
   );
 }
