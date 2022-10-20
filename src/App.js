776e20452a0f5c84b3707ba77d2aa2091dfac3f0
seonhannn <seon.hannn@gmail.com>
2022-10-20 09:40:51 +0900

    error 해결

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import { type } from "@testing-library/user-event/dist/type";
-import styled from "styled-components";
+import React from "react";
 import axios from "axios";
+import {useState} from "react";
 
 function App() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${lotation}&appid=${API_KEY}`;
-  const API_KEY = "614f734515b5ebb1a40e59ef2c368a27";
 
-  const {lotation, setLotation} = useState("");
+  const [location, setLocation] = useState("");
+  const [result, setResult] = useState({});
+  const API_KEY = "614f734515b5ebb1a40e59ef2c368a27";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
 
   const searchWeather = async (e) => {
     if(e.key === 'Enter') {
       try {
         const data = await axios({
-          method: 'get'
+          method: 'get',
           url: url
         })
+        setResult(data);
       } catch (err) {
         alert(err);
       }
@@ -22,35 +24,23 @@ function App() {
   }
 
   return (
-    <AppWrap>
-      <div className="appContentWrap">
-        <input
-        placeholder="도시를 입력하세요"
-        value={lotation}
-        onChange={(e) =>
-          setLotation(e.target.value)
-        }
-        type="text"
-        onKeyDown={searchWeather}
-        >
-        </input>
-      </div>
-    </AppWrap>
+    <div className="App">
+      <input 
+      placeholder="도시를 입력하세요" 
+      value={location}
+      onChange={(e) => setLocation(e.target.value)}
+      type="text"
+      onKeyDown={searchWeather}>
+      </input>
+      {Object.keys(result).length !== 0 && (
+          <div>
+            <div className="city">{result.data.name}</div>
+            <div className="temperature">{result.data.main.temp}</div>
+            <div className="sky">{result.data.weather[0].main}</div>
+          </div>
+        )}
+    </div>
   );
 }
 
 export default App;
-
-const AppWrap = styled.div`
-  width: 100vw;
-  height: 100vh;
-
-  .appContentWrap {
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    position: absolute;
-    border: 1px blue solid;
-    padding: 20px;
-  }
-`;
