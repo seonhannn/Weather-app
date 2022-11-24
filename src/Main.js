95b89ee1a9829a80a1d15e4651ac92986b17d0a9
seonhannn <seon.hannn@gmail.com>
2022-11-24 10:20:39 +0900

    기온에 따라 배경 변경

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,15 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import { useState, useEffect } from "react";
+import moment from "moment";
+import styled from "styled-components";
 import "./Main.css";
 
 function Main() {
+
+    const WeatherImgBox = styled.div`
+        background-color : ${props => props.getWeatherImg === "warm" ? "red" : "blue"};
+    `
 
     const [weather, setWeather] = useState(null);
     const API_key = "614f734515b5ebb1a40e59ef2c368a27";
@@ -21,25 +27,47 @@ function Main() {
     const getWeatherByCurrentLocation = async (lat, lon) => {
         // &units=metric => 섭씨 사용
         let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`;
-        // url에 데이터를 가져올 때까지 기다려 주세요
         let response = await fetch(url);
         let data = await response.json();
         setWeather(data);
     };
-    
+
     useEffect(() => {
         getCurrentLocation();
     }, []);
 
+    const nowTime = moment().format("hh:mm");
+
+    const getWeatherImg = () => {
+        let warm, cold;
+
+        if({weather} >= 14) {
+            return warm;
+        } else {
+            return cold;
+        }
+    }
+
     return (
         <div className="main">
             <div className="main-weather-container">
-                <div className="main-place">{weather?.name}</div>
+                <div className="main-place">
+                    <div className="main-palce-txt">현재 위치</div>
+                    {weather?.name}
+                </div>
+                <div className="main-time">
+                    {nowTime}
+                </div>
                 <div className="main-weather-info">
                     {weather?.main.temp}
-                    <span>도</span>
+                    <span>º</span>
                 </div>
             </div>
+            <WeatherImgBox>
+                <div className="weather-img">
+                    dddddddd
+                </div>
+            </WeatherImgBox>
             <div className="main-btn-container">
                 <button className="main-btn">
                     <Link to={"/Search"} className="main-link">
